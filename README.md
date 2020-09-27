649e368dc95f4b3587a70671768c5faa4c959685
chethanagopinath <chethugopi12@gmail.com>
2020-09-27 03:50:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-l9zjpu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-l9zjpu)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>What do you wanna say to your friend?<span role="img" aria-label="sparkles">✨</span></h1>
+      <textarea placeholder = "Enter text here"></textarea>
+    </div>
+  );
+}
