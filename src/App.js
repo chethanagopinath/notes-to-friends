d735b1a796413048f219fff19cde0496a81eeffc
chethanagopinath <chethugopi12@gmail.com>
2020-09-27 04:49:46 -0400

    Created layout

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import "./style.css";
 export default function App() {
   return (
     <div classname = "main-div">
-      <h1>Notes to friends<span role="img" aria-label="sparkles">✨</span></h1>
+      <h1>What do you wanna say to your friend?<span role="img" aria-label="sparkles">✨</span></h1>
       <form>
-      <textarea placeholder = "What do you wanna say to your friend?"></textarea>
+      <textarea placeholder = "Feel free to add links to cute dog videos too.."></textarea>
       <ul></ul>
-      <input type = "text" placeholder = "Your friend's email" />
+      <input type = "text" placeholder = "Your friend's email id" />
       <ul></ul>
       <input type = "submit" value = "Send email"/>
       </form>
